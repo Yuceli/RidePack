0703bba6cc909695c529bc84c6a6cab8137b9e34
Yussel <rtrtrt_41@hotmail.com>
2015-02-16 14:02:16 -0600

    Ejemplo de uso del modelo
    Puse un pequeño ejemplo de como usar el modelo en el archivo de routes,
    que posteriormente será eliminado.

--- app/routes.php
+++ app/routes.php
@@ -25,3 +25,73 @@ Route::get('/register', function()
 {
 	return View::make('register');
 });
+
+
+Route::get('ejemploModelo', function()
+{
+	$user_id = 7;
+
+	$user = User::find($user_id);
+
+	$res = '<h1>User</h1>';
+	$res .= $user->email . '</br>';
+	$res .= Hash::check('12345', $user->password) . '</br>';
+	$res .= $user->name . '</br>';
+	$res .= $user->last_name . '</br>';
+	$res .= $user->phone . '</br>';
+	$res .= $user->birthdate . '</br>';
+	$res .= $user->city->name . '</br>';
+	$res .= $user->city->country->name . '</br>';
+
+	//Packs
+	$res .= '<h1>Packs</h1>';
+
+	foreach($user->packs as $pack)
+	{
+		$res .= $pack->title . '</br>';
+		$res .= $pack->sending_date . '</br>';
+		$res .= $pack->arrival_date . '</br>';
+		$res .= $pack->reward . '</br>';
+		$res .= $pack->volume . '</br>';
+		$res .= $pack->weight. '</br>';
+		$res .= $pack->observation . '</br>';
+		$res .= $pack->status . '</br>';
+		$res .= $pack->fromCity->name . '</br>';
+		$res .= $pack->fromCity->country->name . '</br>';
+		$res .= $pack->toCity->name . '</br>';
+		$res .= $pack->toCity->country->name . '</br>';
+		$res .= $pack->messages . '</br>';
+		$res .= $pack->requests . '</br>';
+		$res .= $pack->comment->first() . '</br>';
+
+		$res .= '<h3>Associated Trips </h3>';
+		$res .= $pack->trips . '</br>';
+	}
+
+	//Trip
+	$res .= '<h1>Trips</h1>';
+
+	foreach($user->trips as $trip)
+	{
+		$res .= $trip->departure_date . '</br>';
+		$res .= $trip->arrival_date . '</br>';
+		$res .= $trip->carry_reward . '</br>';
+		$res .= $trip->max_volume . '</br>';
+		$res .= $trip->max_weight. '</br>';
+		$res .= $trip->observation . '</br>';
+		$res .= $trip->transport . '</br>';
+		$res .= $trip->departureCity->name . '</br>';
+		$res .= $trip->departureCity->country->name . '</br>';
+		$res .= $trip->arrivalCity->name . '</br>';
+		$res .= $trip->arrivalCity->country->name . '</br>';
+		$res .= $trip->messages . '</br>';
+		$res .= $trip->requests . '</br>';
+		$res .= $trip->comment->first() . '</br>';
+
+		$res .= '<h3>Associated Packs</h3>';
+		$res .= $trip->packs . '</br>';
+	}
+
+
+	return $res;
+});
