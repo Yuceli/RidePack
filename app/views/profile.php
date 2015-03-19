6bb661b5d0c991e972052ddb89a62b7495e311c7
Yuceli <yuceli.polanco@gmail.com>
2015-03-19 15:53:03 -0600

    Cambios en vista upcoming-trips

--- app/views/profile.php
+++ app/views/profile.php
@@ -1,63 +1,58 @@
-<!DOCTYPE html>
+  <!DOCTYPE html>
   <html lang="en">
   <head>
-  <meta charset="UTF-8">
-  <title>RidePack</title>
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.css">
-  <link rel="stylesheet" href="plugins/prism/prism.css">
-  <!-- Theme CSS -->  
-  <link id="theme-style" rel="stylesheet" href="css/styles.css">
-  <link rel="stylesheet" href="css/upcoming-trips.css">
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+    <title>RidePack</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="plugins/prism/prism.css">
+    <!-- Theme CSS -->  
+    <link id="theme-style" rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/upcoming-trips.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
   </head>
   <body>
 
-  <!-- ******HEADER****** --> 
-  <header id="header" class="header">  
-    <div class="container">            
-      <h1 class="logo pull-left">
-        <a class="scrollto" href="#promo">
-          <span class="logo-title">RidePack</span>
-        </a>
-      </h1><!--//logo-->              
-      <nav id="main-nav" class="main-nav navbar-right" role="navigation">
-        <div class="navbar-header">
-          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button><!--//nav-toggle-->
-        </div><!--//navbar-header-->            
-        <div class="navbar-collapse collapse" id="navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active nav-item sr-only"><a class="scrollto" href="#promo">Home</a></li>
-            <li class="nav-item"><a class="scrollto" href="#about">Buscar</a></li>
-            <li class="nav-item"><a class="scrollto" href="#features">Enviar paquete</a></li>                        
-            <li class="nav-item last"><a class="scrollto" href="#contact">Publicar viaje</a></li>
-          </ul><!--//nav-->
-        </div><!--//navabr-collapse-->
-      </nav><!--//main-nav-->
-    </div>
-  </header><!--//header--> 
+    <!-- ******HEADER****** --> 
+    <header id="header" class="header">  
+      <div class="container">            
+        <h1 class="logo pull-left">
+          <a class="scrollto" href="#promo">
+            <span class="logo-title">RidePack</span>
+          </a>
+        </h1><!--//logo-->              
+        <nav id="main-nav" class="main-nav navbar-right" role="navigation">
+          <div class="navbar-header">
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button><!--//nav-toggle-->
+          </div><!--//navbar-header-->            
+          <div class="navbar-collapse collapse" id="navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="active nav-item sr-only"><a class="scrollto" href="#promo">Home</a></li>
+              <li class="nav-item"><a class="scrollto" href="#about">Buscar</a></li>
+              <li class="nav-item"><a class="scrollto" href="#features">Enviar paquete</a></li>                        
+              <li class="nav-item last"><a class="scrollto" href="#contact">Publicar viaje</a></li>
+            </ul><!--//nav-->
+          </div><!--//navabr-collapse-->
+        </nav><!--//main-nav-->
+      </div>
+    </header><!--//header--> 
 
 
-
-  
-
-
-
-
+    
   <br><br><br>
   <!-- ******FOOTER****** --> 
   <footer class="footer">
-  <div class="container text-center">
-    <small class="copyright">Desarrollado con <i class="fa fa-heart"></i> por desarrolladores de RidePack</small>
-  </div><!--//container-->
+    <div class="container text-center">
+      <small class="copyright">Desarrollado con <i class="fa fa-heart"></i> por desarrolladores de RidePack</small>
+    </div><!--//container-->
   </footer><!--//footer-->
 
   <!-- Javascript -->          
