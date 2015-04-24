ef24d3be339f4147987df3e5ce541c63795bea55
bheftye <bheftye92@gmail.com>
2015-04-23 19:34:03 -0500

    CU-20 Responder petición hecha a mi viaje. (T-V)

--- app/controllers/HandleRequestsController.php
+++ app/controllers/HandleRequestsController.php
@@ -13,7 +13,32 @@ class HandleRequestsController extends BaseController {
             ->get();
 		return View::make('handle_requests')->with('requests', $requests);
 	}
+
+	public function acceptRequest($id){
+		$petition = Petition::findorFail($id);
+
+		if($petition -> status == "onhold"){
+			$petition -> status = "accepted";
+			$petition -> save();
+			Session::flash('message','La petición a sido aceptada');
+			Session::flash('class', 'success');
+			return Redirect::to('/management');
+		}
+		if($petition -> status == "accepted"){
+			Session::flash('message','La petición ya habia sido aceptada');
+			Session::flash('class', 'danger');
+			return Redirect::back();
+		}
+		if($petition -> status == "finished"){
+			Session::flash('message','La petición ya no es válida o ha sido terminada');
+			Session::flash('class', 'danger');
+			return Redirect::back();
+		}
+		
+
+
 	}
+}
 
 
 
