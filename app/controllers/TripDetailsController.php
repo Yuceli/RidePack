c3b0466eb2eb26f88c07e2ae66256cd3c1db5ae2
bheftye <bheftye92@gmail.com>
2015-04-23 19:35:26 -0500

    CU-24 Solicitar viajero. (T-V)

--- app/controllers/TripDetailsController.php
+++ app/controllers/TripDetailsController.php
@@ -17,6 +17,38 @@ class TripDetailsController extends BaseController {
 			return View::make('trip_details', compact("trip", "user", "trips"));
 		}
 	}
+
+	public function sendRequest($id)
+	{
+			$myPetitions = Petition::where('from_user', '=', Auth::id())->where('requestable_id', '=', $id)->get();
+			if( sizeof($myPetitions->toArray())==0 )
+			{
+				$petition = new Petition;
+				$petition->from_user = Auth::id();
+				$petition->status = 'onhold';
+				$trip = Trip::find($id);
+
+				if($petition->save() && $trip->requests()->save($petition))
+				{
+					Session::flash('message','La petición a sido registrada');
+					Session::flash('class', 'success');
+					return Redirect::to('/management');
+				}
+				else
+				{
+					Session::flash('message', 'Ocurrió un error con la petición. Intente nuevamente');
+					Session::flash('class', 'danger');
+					return Redirect::back();
+				}
+			}
+			else
+			{
+				Session::flash('message', 'Ya se postuló antes. No puede postularse más de una vez');
+				Session::flash('class', 'danger');
+				return Redirect::back();
+			}
+		
+	}
 }
 
 ?>
